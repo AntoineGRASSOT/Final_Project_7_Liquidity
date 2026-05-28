90e2c43b552198bace5e3274a75165c25637732d
Alessandro Lolli <alessandro.lolli@mail.polimi.it>
2026-05-28 11:34:25 +0200

    function that imported the parameters from the paper delated

--- liquidity/MHWparams_paper.m
+++
@@ -1,8 +0,0 @@
-function MHWparams = MHWparams_paper()
-% MHWPARAMS_PAPER  Volatility parameters from Baviera-Nassigh-Nastasi (2021).
-
-MHWparams.a     = 0.1294;
-MHWparams.sigma = 0.0126;
-MHWparams.gamma = 0.0007;
-
-end
